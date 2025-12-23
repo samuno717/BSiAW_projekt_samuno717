c6f5fda551c5321951c9e22af8bdea2628983615
Michal Frysiek <frymax717@gmail.com>
2025-12-23 16:20:47 +0100

    Launching CI/CD

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     # Tworzenie tabel w bazie danych przy starcie, jeśli nie istnieją
     with app.app_context():
         db.create_all()
-    
+
     # Uruchomienie serwera (host 0.0.0.0 jest wymagany dla Dockera)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True) # nosec B104
