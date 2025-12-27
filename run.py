0e66a4dff5c9af20d29f2c036939efbd1cea5a58
Michal Frysiek <frymax717@gmail.com>
2025-12-27 19:23:38 +0100

    Added password handling by GitHub secrets

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from app import app, db
+import os
 
 if __name__ == '__main__':
     # Tworzenie tabel w bazie danych przy starcie, jeśli nie istnieją
@@ -7,3 +8,6 @@ if __name__ == '__main__':
 
     # Uruchomienie serwera (host 0.0.0.0 jest wymagany dla Dockera)
     app.run(host='0.0.0.0', port=5000, debug=True) # nosec B104
+
+    db_password = os.environ.get('DB_PASSWORD', 'local_pass')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_password}@db_host/dbname'
